8b7a69567d1106aadc75974d44c1c29f0cd63d65
Thiago Melo <thiago.melo@alfalaval.com>
2023-10-01 16:30:24 -0300

    feat: usando métodos

--- execucao.py
+++ execucao.py
@@ -1,15 +1,9 @@
 from conta import Conta
 
 conta = Conta(123, "Thiago", 55.0, 1000.0)
-print(f"Número: {conta.numero}")
-print(f"Titular: {conta.titular}")
-print(f"Saldo: {conta.saldo}")
-print(f"Limite: {conta.limite}")
 print("\n")
-
-
-conta2 = Conta(123, "Marco", 100.0, 1000.0)
-print(f"Número: {conta2.numero}")
-print(f"Titular: {conta2.titular}")
-print(f"Saldo: {conta2.saldo}")
-print(f"Limite: {conta2.limite}")
+conta.extrato()
+conta.deposita(100)
+conta.extrato()
+conta.saca(50)
+conta.extrato()
