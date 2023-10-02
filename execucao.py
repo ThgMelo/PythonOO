fff1533ae17ffefb481def5cb4be28001bbc7af5
Thiago Melo <thiago.melo@alfalaval.com>
2023-10-02 13:37:20 -0300

    feat: encapsulamento

--- execucao.py
+++ execucao.py
@@ -2,5 +2,18 @@ from conta import Conta
 
 conta = Conta(123, "Thiago", 55.0, 1000.0)
 print("\n")
-#print(conta.__limite) aqui lança erro
-print(conta._Conta__limite) # podemos acessar indiretamente
+conta2 = Conta(321, "Marco", 100.0, 1000.0)
+
+valor = 10.0
+
+conta2.saca(valor)
+conta.deposita(valor)
+
+print("\n")
+
+conta.extrato()
+conta2.extrato()
+
+conta.transfere(10, conta2)
+conta.extrato()
+conta2.extrato()
