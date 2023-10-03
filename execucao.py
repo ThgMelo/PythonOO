287d2b90cd116176f67ec0b61f731e5d7c178611
Thiago Melo <thiago.melo@alfalaval.com>
2023-10-03 14:16:48 -0300

    feat: métodos privados

--- execucao.py
+++ execucao.py
@@ -2,12 +2,4 @@ from conta import Conta
 from cliente import Cliente
 
 conta = Conta(123, "Thiago", 55.0, 1000.0)
-conta.limite = 20000
-print(conta.limite)
-print("\n")
-
-cliente = Cliente("thiago")
-print(cliente.nome)
-
-cliente.nome = "duda"
-print(cliente.nome)
+conta.saca(1200)
