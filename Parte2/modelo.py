866383531d95924ea73481042f9f16efdf75d5e4
Thiago Melo <thiago.melo@alfalaval.com>
2023-10-04 10:47:17 -0300

    refact: Reproveitando um list

--- Parte2/modelo.py
+++ Parte2/modelo.py
@@ -41,13 +41,11 @@ class Serie(Programa):
         return f'{self.nome} - {self.ano} - {self.temporadas} temporadas - {self.likes} Likes'
 
 
-class Playlist:
+class Playlist(list):
     def __init__(self, nome, programas):
         self.nome = nome
-        self.programas = programas
+        super().__init__(programas)
 
-    def tamanho(self):
-        return len(self.programas)
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
@@ -64,10 +62,13 @@ vingadores.dar_like()
 atlanta.dar_like()
 atlanta.dar_like()
 
-
 filmes_e_series = [vingadores, atlanta, demolidor, tmep]
 playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
+print(f'Nome do playlist: {playlist_fim_de_semana.nome}')
+print(f'Tamanho do playlist: {len(playlist_fim_de_semana)}')
 
-for programa in playlist_fim_de_semana.programas:
+for programa in playlist_fim_de_semana:
     print(programa)
+
+print(f'Tá ou não tá? {demolidor in playlist_fim_de_semana}')
