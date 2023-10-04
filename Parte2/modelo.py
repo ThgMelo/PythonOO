18c530d979888f52bfea31117f4689a0794a47fa
Thiago Melo <thiago.melo@alfalaval.com>
2023-10-04 10:21:22 -0300

    refact: Representação textual de objetos

--- Parte2/modelo.py
+++ Parte2/modelo.py
@@ -19,8 +19,8 @@ class Programa:
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
 
-    def imprime(self):
-        print(f'{self.nome} - {self.ano} - {self.likes} Likes')
+    def __str__(self):
+        return f'{self.nome} - {self.ano} - {self.likes} Likes'
 
 
 class Filme(Programa):
@@ -28,8 +28,8 @@ class Filme(Programa):
         super().__init__(nome, ano)
         self.duracao = duracao
 
-    def imprime(self):
-        print(f'{self.nome} - {self.ano} - {self.duracao} min - {self.likes} Likes')
+    def __str__(self):
+        return f'{self.nome} - {self.ano} - {self.duracao} min - {self.likes} Likes'
 
 
 class Serie(Programa):
@@ -37,8 +37,8 @@ class Serie(Programa):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
-    def imprime(self):
-        print(f'{self.nome} - {self.ano} - {self.temporadas} temporadas - {self.likes} Likes')
+    def __str__(self):
+        return f'{self.nome} - {self.ano} - {self.temporadas} temporadas - {self.likes} Likes'
 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
@@ -52,4 +52,4 @@ atlanta.dar_like()
 filmes_e_series = [vingadores, atlanta]
 
 for programa in filmes_e_series:
-    programa.imprime()
+    print(programa)
